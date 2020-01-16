37a4f75ad83f738ed5a70fdcedce4296c3f89873
Emmanuel Madeyi <emma.madeyi.nuetion.com>
2020-01-16 11:58:56 +0100

    Updates

---
+++ precache-manifest.d8985d425fba862286b7995c714db449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfee4713a7d553e7ba53ff2ec23d76c",
+    "url": "/conttacts-reactjs/index.html"
+  },
+  {
+    "revision": "2f552ac46f8502c96057",
+    "url": "/conttacts-reactjs/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "73a4fca171ab90cfe52b",
+    "url": "/conttacts-reactjs/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2f552ac46f8502c96057",
+    "url": "/conttacts-reactjs/static/js/2.f6c74172.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/conttacts-reactjs/static/js/2.f6c74172.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73a4fca171ab90cfe52b",
+    "url": "/conttacts-reactjs/static/js/main.f4c0d341.chunk.js"
+  },
+  {
+    "revision": "b18f17cca71663a3019e",
+    "url": "/conttacts-reactjs/static/js/runtime-main.620e492c.js"
+  }
+]);
